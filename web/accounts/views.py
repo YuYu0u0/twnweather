88ed84dc9d29922888e62ebfe58df8b48191057e
YuYu0u0 <mnemonic3310@gmail.com>
2024-06-14 16:27:37 +0800

    留下網頁開發

--- web/accounts/views.py
+++ web/accounts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate, login
 
 
 def register(request):
@@ -14,15 +14,20 @@ def register(request):
             return redirect('login')
     else:
         form = UserCreationForm()
-    return render(request, 'accounts/register.html', {'form': form})
+    return render(request, 'registration/register.html', {'form': form})
 
 
-def login(request):
+def user_login(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            auth_login(request, form.get_user())
-            return redirect('index')  # Redirect to the homepage after login
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # Redirect to the home page or any other page after login
+                return redirect('home')
     else:
         form = AuthenticationForm()
-    return render(request, 'accounts/login.html', {'form': form})
+    return render(request, 'registration/login.html', {'form': form})
