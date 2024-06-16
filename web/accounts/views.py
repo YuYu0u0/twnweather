217095dff278234805051012bf2e47c0d19f9e66
YuYu0u0 <mnemonic3310@gmail.com>
2024-06-16 21:55:10 +0800

    註冊&登入表單

--- web/accounts/views.py
+++ web/accounts/views.py
@@ -4,31 +4,35 @@ from django.shortcuts import render, redirect
 
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import CustomUserCreationForm,LoginForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            print('User registered successfully')
             return redirect('user_login')
+        else:
+            print(form.errors)  # 打印表單錯誤信息
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
 
 
 def user_login(request):
     if request.method == 'POST':
-        MyLoginForm = LoginForm(request.POST)
-        if MyLoginForm.is_valid():
-            username = MyLoginForm.cleaned_data.get('username')
-            password = MyLoginForm.cleaned_data.get('password')
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
                 # Redirect to the home page or any other page after login
                 return redirect('current_weather')
     else:
-        MyLoginForm = LoginForm()
-    return render(request, 'accounts/login.html', {'form': MyLoginForm})
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
