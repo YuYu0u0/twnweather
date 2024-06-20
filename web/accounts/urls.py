75bcf1f2ec178ae296edec66d69c1c40421e95d8
YuYu0u0 <mnemonic3310@gmail.com>
2024-06-20 15:07:01 +0800

    密碼重置信功能建立，目前未有非註冊信箱提醒

--- web/accounts/urls.py
+++ web/accounts/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import register, user_login, user_logout
+from .views import register, user_login, user_logout, reset_password
 
 
 urlpatterns = [
     path('register/', (register), name='register'),
     path('login/', user_login, name='user_login'),
     path('logout/', user_logout, name='logout'),
+    path('reset_password/', reset_password,name='reset_password'),
+     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
--- web/web/settings.py
+++ web/web/settings.py
@@ -129,3 +129,17 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # SESSION_ENGINE
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+
+
+# Email settings
+from dotenv import load_dotenv
+load_dotenv()
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = 'w4da2486@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
