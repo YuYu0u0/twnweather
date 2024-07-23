25751085602dcf16f9b50a660b3cafae3edddc7c
YuYu0u0 <mnemonic3310@gmail.com>
2024-07-23 12:05:38 +0800

    render allow host

--- web/web/settings.py
+++ web/web/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("Django_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['twnweather.onrender.com']
 
 
 # Application definition
