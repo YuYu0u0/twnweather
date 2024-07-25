2a60574043bf4baec30fe23af7e8df294eb177d7
YuYu0u0 <mnemonic3310@gmail.com>
2024-07-25 21:25:02 +0800

    add logo img

--- web/web/settings.py
+++ web/web/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("Django_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['twnweather.onrender.com']
 CSRF_TRUSTED_ORIGINS = ['https://twnweather.onrender.com']
 
 # Application definition
