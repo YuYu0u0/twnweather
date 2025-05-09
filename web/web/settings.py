c8f4dbaa59a1ad1686c97d0064187eaa088d5d8d
YuYu0u0 <mnemonic3310@gmail.com>
2025-05-09 11:59:05 +0800

    change host settings

--- web/web/settings.py
+++ web/web/settings.py
@@ -27,10 +27,9 @@ SECRET_KEY = os.getenv("Django_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['twnweather.onrender.com',
-                 'weatherwebsite-r312.onrender.com']
+ALLOWED_HOSTS = ['twnweather.onrender.com']
 CSRF_TRUSTED_ORIGINS = [
-    'https://twnweather.onrender.com', 'https://weatherwebsite-r312.onrender.com']
+    'https://twnweather.onrender.com']
 
 # Application definition
 
